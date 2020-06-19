e34f71eb058ec1c97b2adb22336fa9b2f8aa191a
larwat <lwtech20@gmail.com>
2020-06-18 20:24:49 -0700

    form skeleton complete

--- rent-tech/src/App.js
+++ rent-tech/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
-import ItemList from "./components/ItemList";
 import styled from "styled-components";
+import ItemList from "./components/ItemList";
+import Form from "./components/Form"; 
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'; 
+import * as yup from "yup";
 
 const Header = styled.h1`
 font-size: 120px;
@@ -12,7 +15,7 @@ const App = () => {
   return (
     <div className="App">
       <Header>Items for Rent</Header>
-      <ItemList />
+      <Form />
     </div>
   );
 };
