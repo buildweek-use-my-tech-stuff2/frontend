f516b808cdb897df9cae9a9fd9e723e34ae4e658
larwat <lwtech20@gmail.com>
2020-06-18 20:41:00 -0700

    updated itemcard to display all fields

--- rent-tech/src/Components/ItemCard.js
+++ rent-tech/src/Components/ItemCard.js
@@ -11,12 +11,15 @@ const ContentWrapper = styled.div`
 const ItemCard = props => {
   return (
     <ContentWrapper>
+      <h2>NAME:</h2>
       <h2>IMAGE HERE</h2>
-      <h2>TYPE:</h2>
       <h4>PRICE:</h4>
-      <h4>LOCATION:</h4>
-      <h4>AVAILABILITY:</h4>
       <h4>DESCRIPTION:</h4>
+      <h4>LOCATION:</h4>
+      <h4>TYPE:</h4>
+      <h4>DEPOSIT:</h4>
+      <h4>RENTER:</h4>
+
     </ContentWrapper>
   );
 };
