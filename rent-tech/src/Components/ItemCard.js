b510ed512e8cce0f948c75e77cfb540ac331d563
fadygouda <fadygouda123@gmail.com>
2020-06-19 22:59:04 -0700

    completed link of larry's components to the api

--- rent-tech/src/Components/ItemCard.js
+++ rent-tech/src/Components/ItemCard.js
@@ -1,57 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
-import { useParams } from 'react-router-dom'
-
-
 
 const ContentWrapper = styled.div`
   border: 2px solid;
   width: 20%;
   background-color:#ECECEC;
   margin-bottom: 30px;
+  display: flex,
+  flex-direction: column,
+  
+  
 `;
 
-const ItemCard = props => {
-  // console.log(props.rentItem)
+const Image = styled.img`
+  height: 100px
+  `
 
-  // const itemMap = props.rentItem.map(array => {
-  //   return array;
-  // })
-  // console.log(itemMap)
+const ItemCard = ({ item }) => {
 
-  const params= useParams();
-  const id = params.itemId
-  const [item, setItem] = useState({
-    name: '',
-    image_url: '',
-    price: '',
-    description: '',
-    location: '',
-    type: '',
-    deposit: '',
-    renter: ''
-  })
+  const { name, image_url, price, description, location, type, deposit, renter} = item;
 
-  useEffect(() => {
-    axiosWithAuth()
-      .get(`/api/items`)
-      .then(res => {
-        console.log(res.data)
-        setItem(res.data.name)
-
-      })
-  }, [])
   return (
     <ContentWrapper>
-      <h2>Name:</h2>
-      <h2>IMAGE HERE</h2>
-      <h4>PRICE:</h4>
-      <h4>DESCRIPTION:</h4>
-      <h4>LOCATION:</h4>
-      <h4>TYPE:</h4>
-      <h4>DEPOSIT:</h4>
-      <h4>RENTER:</h4>
+      <h2>{name}</h2>
+      <Image src={image_url} />
+      <h4>Price to Rent:  {price}</h4>
+      <h4>Description:  {description}</h4>
+      <h4>Location:   {location}</h4>
+      <h4>{type}</h4>
+{      <h4>Deposit:   {deposit}:</h4>
+}      <h4>Lender:    {renter}</h4>
 
     </ContentWrapper>
   );
